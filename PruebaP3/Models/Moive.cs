c7b8ffd93968c6e4635924711039c9380991740b
Enzo Cortez <enzo.cortez@udla.edu.ec>
2025-01-21 07:37:10 -0500

    Creacion de modelos y correcion de errores..

---
+++ PruebaP3/Models/Moive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PruebaP3.Models
+{
+    
+
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Actor { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+        public string Ecortez { get; set; } = "Ecortez";
+    }
+
+}
