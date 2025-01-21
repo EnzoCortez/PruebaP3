1acb5b2bdf403910560e240e7717e4f65156c705
Enzo Cortez <enzo.cortez@udla.edu.ec>
2025-01-21 07:17:08 -0500

    Agregar archivos de proyecto.

---
+++ PruebaP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PruebaP3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaP3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
