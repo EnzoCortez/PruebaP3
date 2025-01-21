57819a6971daa2e02c2ab1e398503a994f0568a3
Enzo Cortez <enzo.cortez@udla.edu.ec>
2025-01-21 07:43:34 -0500

    Creacion de los servicios para el manejo de apis y guardado de datos

---
+++ PruebaP3/Services/DataBaseServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PruebaP3.Models;
+using SQLite;
+
+
+namespace PruebaP3.Services
+{
+    
+
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public DatabaseService(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Movie>().Wait();
+        }
+
+        public Task<List<Movie>> GetMoviesAsync() => _database.Table<Movie>().ToListAsync();
+
+        public Task<int> SaveMovieAsync(Movie movie) => _database.InsertAsync(movie);
+    }
+
+}
---
+++ PruebaP3/Services/MovieService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using PruebaP3.Models;
+
+/*Creacion del servicio para el consumo de la Api */
+namespace PruebaP3.Services
+{
+   
+    public class MovieService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public async Task<Movie> SearchMovieAsync(string query)
+        {
+            string url = $"https://freetestapi.com/api/v1/movies?search={query}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            var movies = JsonSerializer.Deserialize<List<MovieResponse>>(response);
+
+            if (movies?.Count > 0)
+            {
+                var movie = movies[0];
+                return new Movie
+                {
+                    Title = movie.Title,
+                    Genre = movie.Genres?[0] ?? "N/A",
+                    Actor = movie.Actors?[0] ?? "N/A",
+                    Awards = movie.Awards,
+                    Website = movie.Website
+                };
+            }
+            return null;
+        }
+    }
+
+    
+
+}
